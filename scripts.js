07c54e2259a684c1d1d7b0cddfdb111edfb65976
chris long <christopher.long@student.swic.edu>
2020-10-06 17:07:27 -0500

    Send name value to console.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 const form = document.querySelector("form");
 
-form.addEventListener("click", function () {});
+form.addEventListener("submit", () => {
+  event.preventDefault();
+  console.log(event.target.elements[0].value);
+});
 
 //DO When clicked have it return a box saying "name" has "#" beef and "#" hogs scheduled for "date"
