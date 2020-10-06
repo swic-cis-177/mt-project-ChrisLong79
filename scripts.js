90316c874713878c802012287312debaa4ace337
chris long <christopher.long@student.swic.edu>
2020-10-06 17:19:49 -0500

    grab data from form

--- scripts.js
+++ scripts.js
@@ -2,7 +2,14 @@ const form = document.querySelector("form");
 
 form.addEventListener("submit", () => {
   event.preventDefault();
-  console.log(event.target.elements[0].value);
+
+  const data = {};
+  for (let i = 0; i <= 4; i++) {
+    console.log(event.target.elements[i].value);
+  }
 });
 
-//DO When clicked have it return a box saying "name" has "#" beef and "#" hogs scheduled for "date"
+// phone # event.target.elements[1].value)
+// beef # event.target.elements[2].value)
+// hog # event.target.elements[3].value)
+// date event.target.elements[4].value)
