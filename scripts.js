9798dc3541f9a042d0517f480635607ccdb7c944
chris long <christopher.long@student.swic.edu>
2020-10-08 20:47:01 -0500

    Edit notes in scripts.js file

--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
 const form = document.querySelector("form");
 
+//Listen for the submit event
 form.addEventListener("submit", () => {
+  //Block default handling of the submit action
   event.preventDefault();
-
+  //Declare a variable for the form data
   const formData = {};
+  //Create loop to display data from form
   for (let i = 0; i <= 4; i++) {
-    formData[event.target.elements[i].id] = event.target.elements[i].value;
     formData[event.target.elements[i].id] = event.target.elements[i].value;
   }
   console.log(formData);
