d0e4967c66e7a0c9e8240d99e5d3e3922b714c12
ChrisLong79 <christopher.long@student.swic.edu>
2020-12-06 15:42:21 -0600

    added todo notes

--- lib.js
+++ lib.js
@@ -3,15 +3,42 @@ const template = document.querySelector("template");
 const form = document.querySelector("form");
 const div = document.querySelector("div");
 
-export const createShoppingCart = (salesData) => {
+// TODO: create const to have unit price =10.00 for each id
+const unitPrice = 10.0;
+
+// TODO: create const Total to multiply unit price by qty in input box.
+// TODO: create const that adds all Totals up and have it displayed in H3  "total $" grand total.
+// flavor = id ,quantity= id.value, unit price=const unitPrice , total=const total
+
+// fix code in createShoppingCart
+export const createShoppingCart = (shoppingCart) => {
   tbody.innerHTML = null;
 
-  salesData.forEach(({ flavor, quantity, total }) => {
+  shoppingCart.forEach(({ flavor, quantity, total }) => {
     const newSalesRow = template.content.cloneNode(true);
     const newSalesTDs = newSalesRow.querySelectorAll("td");
-    newSalesTDs[0].innerText = flavor;
+    newSalesTDs[0].innerText = "id";
     newSalesTDs[1].innerText = quantity;
     newSalesTDs[2].innerText = total;
     tbody.appendChild(newSalesRow);
   });
 };
+
+const processForm = (elements) =>
+  Array.from(elements)
+    .filter(({ id }) => id)
+    .reduce(
+      (formInfo, { id, value }) => ({
+        ...formInfo,
+        [id]: value,
+      }),
+      {}
+    );
+
+//what i want form to do.
+// allow user to select quantity of smokies by flavor +
+//create a const to calculate total for each flavor quantity *10.00
+//create a const to properly display id qty and the total into form on click of add to cart button.
+
+// only allow for form to be submitted once.
+// calculate grand total below form by adding all the totals together.
